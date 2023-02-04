706b0f7de7fd1fcd02974f7ea239315d2a52d38c
teryanarmen <61996358+teryanarmen@users.noreply.github.com>
2023-02-03 20:48:52 -0800

    Update verifyPatch5.sh

--- certora/scripts/verifyPatch5.sh
+++ certora/scripts/verifyPatch5.sh
@@ -1,22 +1,16 @@
-#!/bin/sh
 
-f="certora/tests/certora/bug5.patch"
+certoraRun  certora/harnesses/SyndicateHarness.sol \
+    certora/harnesses/MockStakeHouseUniverse.sol \
+    certora/harnesses/MockStakeHouseRegistry.sol \
+    certora/harnesses/MockSlotSettlementRegistry.sol \
+    certora/harnesses/MocksETH.sol \
+    --verify SyndicateHarness:certora/specs/Syndicate.spec \
+    --cloud master \
+    --optimistic_loop \
+    --optimize 1 \
+    --loop_iter 3 \
+    --rule getUnprocessedETHForAllCollateralizedSlot_dependsOnNumberOfKnots \
+    --rule_sanity \
+    --settings -optimisticFallback=true \
+    --packages @blockswaplab=node_modules/@blockswaplab @openzeppelin=node_modules/@openzeppelin \
 
-if [[ "$1" ]]
-then
-    MSG="$1"
-else
-    MSG="msg"
-fi
-
-echo "Applying $f"
-git apply $f;
-echo "Running script"
-# for script in certora/scripts/*.sh
-# do
-#     echo "Running $script"
-#     sh $script
-# done
-certora/scripts/verifySyndicate.sh "$MSG" getUnprocessedETHForAllCollateralizedSlot_dependsOnNumberOfKnots
-echo "Reverting $f"
-git apply -R $f
