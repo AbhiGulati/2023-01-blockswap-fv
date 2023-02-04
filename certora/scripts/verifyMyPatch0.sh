408fc071f959190a3b9f0bad96db2b9767017d51
teryanarmen <61996358+teryanarmen@users.noreply.github.com>
2023-02-03 20:50:35 -0800

    Update verifyMyPatch0.sh

--- certora/scripts/verifyMyPatch0.sh
+++ certora/scripts/verifyMyPatch0.sh
@@ -1,22 +1,16 @@
-#!/bin/sh
 
-f="certora/tests/participants/bug0.patch"
 
-if [[ "$1" ]]
-then
-    MSG="$1"
-else
-    MSG="msg"
-fi
-
-echo "Applying $f"
-git apply $f;
-echo "Running script"
-# for script in certora/scripts/*.sh
-# do
-#     echo "Running $script"
-#     sh $script
-# done
-certora/scripts/verifySyndicate.sh "$MSG" claimAsStakerUpdatesAccrued
-echo "Reverting $f"
-git apply -R $f
+certoraRun  certora/harnesses/SyndicateHarness.sol \
+    certora/harnesses/MockStakeHouseUniverse.sol \
+    certora/harnesses/MockStakeHouseRegistry.sol \
+    certora/harnesses/MockSlotSettlementRegistry.sol \
+    certora/harnesses/MocksETH.sol \
+    --verify SyndicateHarness:certora/specs/Syndicate.spec \
+    --cloud master \
+    --optimistic_loop \
+    --optimize 1 \
+    --loop_iter 3 \
+    --rule claimAsStakerUpdatesAccrued \
+    --rule_sanity \
+    --settings -optimisticFallback=true \
+    --packages @blockswaplab=node_modules/@blockswaplab @openzeppelin=node_modules/@openzeppelin \
